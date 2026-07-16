d4e7e004e42bcf5728b44bbafda829aa04244c2a
Amethnb2218 <amethsl2218@gmail.com>
2026-07-16 08:33:49 +0000

    Add 1000+ questions - 15 full practice exams available
    Added question banks 9-24 covering all SAA-C03 domains:
    security, resilient architectures, high-performing tech,
    and cost optimization. Total: 1005 questions.

--- src/data/allQuestions.js
+++ src/data/allQuestions.js
@@ -6,6 +6,22 @@ import { questionBank5 } from './questionBank5'
 import { questionBank6 } from './questionBank6'
 import { questionBank7 } from './questionBank7'
 import { questionBank8 } from './questionBank8'
+import { questionBank9 } from './questionBank9'
+import { questionBank10 } from './questionBank10'
+import { questionBank11 } from './questionBank11'
+import { questionBank12 } from './questionBank12'
+import { questionBank13 } from './questionBank13'
+import { questionBank14 } from './questionBank14'
+import { questionBank15 } from './questionBank15'
+import { questionBank16 } from './questionBank16'
+import { questionBank17 } from './questionBank17'
+import { questionBank18 } from './questionBank18'
+import { questionBank19 } from './questionBank19'
+import { questionBank20 } from './questionBank20'
+import { questionBank21 } from './questionBank21'
+import { questionBank22 } from './questionBank22'
+import { questionBank23 } from './questionBank23'
+import { questionBank24 } from './questionBank24'
 
 export const allQuestions = [
   ...questions,
@@ -16,6 +32,22 @@ export const allQuestions = [
   ...questionBank6,
   ...questionBank7,
   ...questionBank8,
+  ...questionBank9,
+  ...questionBank10,
+  ...questionBank11,
+  ...questionBank12,
+  ...questionBank13,
+  ...questionBank14,
+  ...questionBank15,
+  ...questionBank16,
+  ...questionBank17,
+  ...questionBank18,
+  ...questionBank19,
+  ...questionBank20,
+  ...questionBank21,
+  ...questionBank22,
+  ...questionBank23,
+  ...questionBank24,
 ]
 
 export function shuffleArray(arr) {
